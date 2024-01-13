f78345233cef937abbb2c0ba8ca4739cb2da9669
Kanzen Minarai <kanzenminarai@proton.me>
2024-01-13 14:11:05 -0400

    created the file to handle the input

---
+++ src/input.lua
@@ -0,0 +1,10 @@
+operator, number1, number2 = 0, 0, 0
+
+function getInput()
+  print("insert the first number: ")
+  number1 = tonumber(io.read())
+  print("insert the operador: ")
+  operator = io.read()
+  print("insert the second number: ")
+  number2 = tonumber(io.read())
+end
